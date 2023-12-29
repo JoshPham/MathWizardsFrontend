18ff594b52c067c4c9039c656a7cf5ee9f1ee8f7
JoshPham <JTPham911@gmail.com>
2023-12-29 00:37:14 +0000

    changed navbar, website font, and logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,41 @@
-import "./NavbarStyles.css";
+import React from 'react';
+import './NavbarStyles.css';
+import logo from './logo192.png';
+
 
 export default function Navbar() {
-    return (
-        <>
-            <nav>
-                <ul>
-                    <div className="item-1">
-                        <li>
-                            <a href="/grades/" id="grade">Grades ⮟</a>
-                            <ul className="dropdown">
-                                <li><a href="/kindergarten/">Kindergarten</a></li>
-                                <li><a href="/firstgrade/">First Grade</a></li>
-                                <li><a href="/secondgrade/">Second Grade</a></li>
-                                <li><a href="/thirdgrade/">Third Grade</a></li>
-                                <li><a href="/fourthgrade/">Fourth Grade</a></li>
-                                <li><a href="/fifthgrade/">Fifth Grade</a></li>
-                            </ul>
-                        </li>
-                        <li className="logo"><a href="/">LOGO Math Wizards</a></li>
-                    </div>
-                    <div className="item-2">
-                        <li><a href="/about/">About Us</a></li>
-                        <li><a href="/settings/">Settings</a></li>
-                    </div>
-                </ul>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav>
+        <div class="logo">
+          <div class="logo-elements">
+            <a href="/">
+              <img src={logo} alt="LOGO" />
+              <span>Math Wizards</span>
+            </a>
+           
+            <div class="item-2"></div>
+          </div>
+        </div>
+        <ul className="menu">
+          <li><a href="/">Home</a></li>
+          <li>
+            <a href="/grades/" id="grade">Grades ⮟</a>
+            <ul className="dropdown">
+              <li><a href="/kindergarten/">Kindergarten</a></li>
+              <li><a href="/firstgrade/">First Grade</a></li>
+              <li><a href="/secondgrade/">Second Grade</a></li>
+              <li><a href="/thirdgrade/">Third Grade</a></li>
+              <li><a href="/fourthgrade/">Fourth Grade</a></li>
+              <li><a href="/fifthgrade/">Fifth Grade</a></li>
+            </ul>
+          </li>
+          <li><a href="/help/">Help Desk</a></li>
+          <li><a href="/login/">Login</a></li>
+          <li><a href="/register/">Register</a></li>
+          <li><a href="/settings/">Settings</a></li>
+        </ul>
+      </nav>
+    </>
+  );
 }
