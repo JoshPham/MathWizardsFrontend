7c01ee484bc4f7f07b0fd9d7187a8a00d9f1112f
JoshPham <JTPham911@gmail.com>
2023-12-29 03:00:32 +0000

    made grade page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Settings from './pages/Settings';
 import Grades from './pages/grades/Grades';
-import Kindergarten from './pages/grades/Kindergarten';
-import FirstGrade from './pages/grades/FirstGrade';
-import SecondGrade from './pages/grades/SecondGrade';
-import ThirdGrade from './pages/grades/ThirdGrade';
-import FourthGrade from './pages/grades/FourthGrade';
-import FifthGrade from './pages/grades/FifthGrade';
+import GradePage from './pages/grades/GradePage';
 import NoPage from './pages/NoPage';
 import axios from 'axios';
-import React from 'react';
 import "./App.css";
 
 class App extends React.Component {
-  state = { grades: [] }
+  state = { grades: [] };
 
   async componentDidMount() {
     try {
-      const gradeResponse = await axios.get('http://localhost:8000/grade/');
+      const gradeResponse = await axios.get('http://localhost:8000/grades/');
       const grades = gradeResponse.data;
       this.setState({ grades });
-  
+
       // Fetch unit data for each grade
       for (const grade of grades) {
         const unitResponse = await axios.get(`http://localhost:8000/units/${grade.grade_id}/`);
         const units = unitResponse.data;
-  
+
         const updatedGrades = this.state.grades.map(g => {
           if (g.grade_id === grade.grade_id) {
             return { ...g, units: units };
           }
           return g;
         });
-  
+
         this.setState({
-          grades: updatedGrades
+          grades: updatedGrades,
         });
       }
     } catch (error) {
@@ -46,21 +41,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route index element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Settings" element={<Settings />} />
           <Route path="/Grades" element={<Grades data={this.state.grades} />} />
-          <Route path="/Kindergarten" element={<Kindergarten />} />
-          <Route path="/FirstGrade" element={<FirstGrade />} />
-          <Route path="/SecondGrade" element={<SecondGrade />} />
-          <Route path="/ThirdGrade" element={<ThirdGrade />} />
-          <Route path="/FourthGrade" element={<FourthGrade />} />
-          <Route path="/FifthGrade" element={<FifthGrade />} />
+          <Route path="/grades/:gradeNumber" element={<GradePage grades={this.state.grades} />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,12 +22,12 @@ export default function Navbar() {
           <li>
             <a href="/grades/" id="grade">Grades ⮟</a>
             <ul className="dropdown">
-              <li><a href="/kindergarten/">Kindergarten</a></li>
-              <li><a href="/firstgrade/">First Grade</a></li>
-              <li><a href="/secondgrade/">Second Grade</a></li>
-              <li><a href="/thirdgrade/">Third Grade</a></li>
-              <li><a href="/fourthgrade/">Fourth Grade</a></li>
-              <li><a href="/fifthgrade/">Fifth Grade</a></li>
+              <li><a href="/grades/0/">Kindergarten</a></li>
+              <li><a href="/grades/1/">First Grade</a></li>
+              <li><a href="/grades/2/">Second Grade</a></li>
+              <li><a href="/grades/3/">Third Grade</a></li>
+              <li><a href="/grades/4/">Fourth Grade</a></li>
+              <li><a href="/grades/5/">Fifth Grade</a></li>
             </ul>
           </li>
           <li><a href="/help/">Help Desk</a></li>
